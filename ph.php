773139ed330418b85525f4d94527af6884f572d5
uchebassey <129692419+uchebassey@users.noreply.github.com>
2023-04-16 11:53:26 +0100

    Add files via upload

---
+++ ph.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src='reverese'></script>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title></title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='open'></script>
+</head>
+<body>
+    <script>
+        HTMLTableRowElement
+        HTMLAudioElement
+        h$
+        HTMLAreaElement
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+           <head>
+               <meta charset="utf-8">
+               <meta name="viewport" content="width=device-width, initial-scale=1.0">
+               <meta http-equiv="X-UA-Compatible" content="IE=edge">
+               <title>Title</title>
+               <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+               <link rel="stylesheet" href="">
+           </head>
+                   
+           <body>
+                   
+           </body>
+                   
+        </html>
+    </script>
+</body>
+<head>
+               <meta charset="utf-8">
+               <meta name="viewport" content="width=device-width, initial-scale=1.0">
+               <meta http-equiv="X-UA-Compatible" content="IE=edge">
+               <title>Title</title>
+               <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+               <link rel="stylesheet" href="">
+</html>
+<head>
+               <meta charset="utf-8">
+               <meta name="viewport" content="width=device-width, initial-scale=1.0">
+               <meta http-equiv="X-UA-Compatible" content="IE=edge">
+               <title>Title</title>
+               <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+               <link rel="stylesheet" href="">
+               <h</html>
+               </html>
+               jsdelivr
+               
+               058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW
+               <caption>
+               <script src=""></script>
+               </caption>
